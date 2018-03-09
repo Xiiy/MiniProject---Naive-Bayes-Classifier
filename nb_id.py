ae218471e1cc47647513a9481fe794479bbf24ed
Xiiy <samaraxiang@yahoo.com>
2018-03-08 23:28:08 -0400

    first commit

---
+++ nb_id.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+def NBAccuracy(features_train,labels_train,features_test,labels_test):
+    from sklearn.naive_bayes import GaussianNB
+    # Create classifier 
+    clf = GaussianNB()
+    # fit the classifier on the training features and labels 
+    clf.fit(features_train,labels_train) 
+    ## use the trained classifier to predict labels for the test features 
+    pred = clf.predict(features_test)
+    ##  calculate and return the accuracy on the test data 
+    count = 0
+    for i in range(len(pred)):
+        if pred[i] == labels_test[i]:
+            count += 1
+    accuracy = (float(count) / len(pred)) 
+    #accuracy = clf.score(features_test, labels_test) 
+    return accuracy
+NBAccuracy(features_train,labels_train,features_test,labels_test)
+
+#########################################################
+
+
